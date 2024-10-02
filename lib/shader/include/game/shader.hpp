4580037085e5f4370d99f0e1630d44b09cda0a05
Gabriel <41166074+gckopper@users.noreply.github.com>
2024-10-02 14:25:37 -0300

    adding shader compilation step

--- lib/shader/include/game/shader.hpp
+++ lib/shader/include/game/shader.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glad/glad.h>
+
 #include <string>
 
 namespace gm {
@@ -13,5 +15,9 @@ namespace gm {
 const std::string SHADER_VERSION = "#version 330 core\n";
 
 std::string load_shader(std::string filename);
+
+GLuint compile_shader(std::string shader_code, GLenum shader_type);
+
+GLuint make_shader_program(std::string vert_path, std::string frag_path);
 
 }
--- lib/shader/src/shader.cpp
+++ lib/shader/src/shader.cpp
@@ -24,3 +24,50 @@ std::string gm::load_shader(std::string filename) {
 
     return result;
 }
+
+GLuint gm::compile_shader(std::string shader_code, GLenum shader_type) {
+    GLuint shader = glCreateShader(shader_type);
+    if (shader == 0) {
+        LOG("Unable to create shader object");
+        terminate();
+    }
+    const GLchar *cstr_source = const_cast<const GLchar *>(shader_code.c_str());
+    glShaderSource(shader, 1, &cstr_source, NULL);
+    glCompileShader(shader);
+    GLint success = 0;
+    glGetShaderiv(shader,  GL_COMPILE_STATUS, &success);
+    // compilation failed
+    if (!success) {
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &success);
+        std::string log(success, ' ');
+        glGetShaderInfoLog(shader, success, NULL, log.data());
+        LOG("Shader compilation failed with error: " << log);
+        terminate();
+    }
+    return shader;
+}
+
+GLuint gm::make_shader_program(std::string vert_path, std::string frag_path) {
+    GLuint vert_shader = compile_shader(load_shader(vert_path), GL_VERTEX_SHADER);
+    GLuint frag_shader = compile_shader(load_shader(frag_path), GL_FRAGMENT_SHADER);
+
+    GLint success = 0;
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vert_shader);
+
+    glAttachShader(shaderProgram, frag_shader);
+
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &success);
+        std::string log(success, ' ');
+        glGetProgramInfoLog(shaderProgram, success, NULL, log.data());
+        LOG("Shader linking failed with error: " << log);
+        terminate();
+    }
+    
+    return shaderProgram;
+}
