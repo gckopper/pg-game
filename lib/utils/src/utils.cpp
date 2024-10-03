cff8709a0d64fb01d090eb4c3969916593264d03
Gabriel <41166074+gckopper@users.noreply.github.com>
2024-10-03 02:55:50 -0300

    adding a GLFW setup function inside the new utils library

---
+++ lib/utils/src/utils.cpp
@@ -0,0 +1,27 @@
+#include <game/utils.hpp>
+
+#include <game/log.hpp>
+#include <cstdlib>
+
+GLFWwindow* gm::setup_glfw(int width, int height, std::string title) {
+    if (glfwInit() == GLFW_FALSE) {
+        LOG("Failed to create GLFW context");
+        terminate();
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    GLFWwindow* window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+    if (gladLoadGL() == 0) {
+        LOG("Failed to initialize OpenGL context");
+        terminate();
+    }
+    int fb_width, fb_height;
+    glfwGetFramebufferSize(window, &fb_width, &fb_height);
+	glViewport(fb_width, fb_height, fb_width, fb_height);
+    return window;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <game/time.hpp>
+#include <game/utils.hpp>
+
+#include <GLFW/glfw3.h>
 
 int main() {
     gm::duration delta_time;
     gm::time_point time, last_tick_time{gm::get_time()};
 
-    while (true) {
+    GLFWwindow* window = gm::setup_glfw(1280, 720, "PG Game");
+
+    while (!glfwWindowShouldClose(window)) {
         time = gm::get_time();
         delta_time = time - last_tick_time;
 
-        // render(delta_time.count());
+        // render(delta_time.count(), window);
 
         if (delta_time >= gm::TICK_STEP) {
             // update_state();
