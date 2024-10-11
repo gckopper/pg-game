dfd1d3d38b955e09ff7257a150a5a48d3115dc6a
LucasS2002 <sauerlucas@edu.unisinos.br>
2024-10-10 23:54:48 -0300

    renaming and updating glfw_setup to better reflect its functionalities

--- lib/utils/include/game/utils.hpp
+++ lib/utils/include/game/utils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <string>
 
 #include <glad/glad.h>
@@ -7,10 +8,13 @@
 
 namespace gm {
 
+const uint16_t WINDOW_WIDTH  = 1280;
+const uint16_t WINDOW_HEIGHT = 720;
+
 const GLfloat WORLD_WIDTH  = 640;
 const GLfloat WORLD_HEIGHT = 360;
 
-GLFWwindow* setup_glfw(int width, int height, std::string title);
+GLFWwindow* init_context(int width, int height, std::string title);
 void terminate();
 
 void glfw_error_callback(int code, const char* desc);
