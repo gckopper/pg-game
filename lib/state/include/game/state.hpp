0a65891f7c80b313079b16654f45bbcb94e3bc96
LucasS2002 <sauerlucas@edu.unisinos.br>
2024-10-11 10:08:26 -0300

    adding collision detection function and demonstration

--- lib/state/include/game/state.hpp
+++ lib/state/include/game/state.hpp
@@ -6,4 +6,12 @@ namespace gm {
 
 void update_vbo(entities& entities);
 
+template<class T, class U>
+constexpr bool colliding(T first, U second) {
+    return first.world_pos.x < second.world_pos.x  + second.hitbox_width  &&
+           first.world_pos.x + first.hitbox_width  > second.world_pos.x   &&
+           first.world_pos.y < second.world_pos.y  + second.hitbox_height &&
+           first.world_pos.y + first.hitbox_height > second.world_pos.y;
+}
+
 }
