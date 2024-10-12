52d9bda93bd6f3a69e048c2bd28994c366c0101f
Gabriel <41166074+gckopper@users.noreply.github.com>
2024-10-12 20:17:50 -0300

    add render library; add background type; add background shader

---
+++ lib/render/include/game/render.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <game/types.hpp>
+
+namespace gm {
+    void render(uint64_t time_delta, Entities& entities, Background& background);
+}
--- lib/types/src/types.cpp
+++ lib/types/src/types.cpp
@@ -11,7 +11,13 @@
 #include <game/utils.hpp>
 
 void gm::setup_entities(Entities& entities) {
-    entities.shader_program = gm::make_shader_program("entities.vert", "entities.frag");
+    entities.shader_program = gm::make_shader_program("entities.vert", "main.frag");
+
+    // cache uniform location
+    entities.u_time_delta = glGetUniformLocation(entities.shader_program, "delta_time");
+    // set the shader sampler to always use the first texture unit
+    glUniform1i(glGetUniformLocation(entities.shader_program, "texture_uniform"), 0);
+
     entities.texture = gm::load_texture("./assets/atlas.png");
     entities.enemy_count = 0;
 
@@ -70,6 +76,50 @@ void gm::setup_entities(Entities& entities) {
     glGenBuffers(1, &entities.ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, entities.ebo);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, entities.ebo_data.size() * sizeof(GLushort), entities.ebo_data.data(), GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void gm::setup_background(Background& background) {
+    background.shader_program = gm::make_shader_program("background.vert", "main.frag");
+
+    // cache uniform location
+    background.u_time_delta = glGetUniformLocation(background.shader_program, "delta_time");
+    // set the shader sampler to always use the first texture unit
+    glUniform1i(glGetUniformLocation(background.shader_program, "texture_uniform"), 0);
+
+    background.texture = gm::load_texture("./assets/background.png");
+    background.vbo_data = {
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,   // bottom left
+            -1.0f,  1.0f, 0.0f, 0.0f, 0.0f, 3.6f,   // top left
+             1.0f,  1.0f, 0.0f, 0.0f, 6.4f, 3.6f,   // top right
+             1.0f, -1.0f, 0.0f, 0.0f, 6.4f, 0.0f,   // bottom right
+    }; 
+    constexpr std::array<GLushort, 6> ebo_data = {
+        0, 1, 2, 0, 2, 3
+    };
+
+    glGenVertexArrays(1, &background.vao);
+    glBindVertexArray(background.vao);
+
+    GLuint ebo;
+    glGenBuffers(1, &background.vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, background.vbo);
+    glBufferData(GL_ARRAY_BUFFER, background.vbo_data.size() * sizeof(GLfloat), background.vbo_data.data(), GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) 0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (2 * sizeof(GLfloat)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*) (4 * sizeof(GLfloat)));
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, ebo_data.size() * sizeof(GLushort), ebo_data.data(), GL_STATIC_DRAW);
 
     glBindVertexArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
