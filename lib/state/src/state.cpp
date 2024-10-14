f4001ffd951f8f7f9014a78bd5581ba4a92c949e
LucasS2002 <sauerlucas@edu.unisinos.br>
2024-10-14 09:49:12 -0300

    adding enemy movement and collision

--- lib/state/src/state.cpp
+++ lib/state/src/state.cpp
@@ -1,11 +1,99 @@
+#include <algorithm>
 #include <cstdint>
 #include <span>
 
 #include <glad/glad.h>
 
-#include <game/types.hpp>
 #include <game/sprite.hpp>
 #include <game/state.hpp>
+#include <game/types.hpp>
+#include <game/utils.hpp>
+
+void gm::move_enemies(Entities &entities) {
+    // update movement from last tick
+    for (uint8_t i = 0; i < entities.enemy_count; ++i) {
+        entities.enemies[i].world_pos += entities.enemies[i].delta_pos;
+        entities.enemies[i].tex_pos   += entities.enemies[i].delta_pos;
+    }
+    
+    // sort by distance to player
+    std::sort(entities.enemies.begin(), entities.enemies.begin() + entities.enemy_count, [&](const Enemy& lhs, const Enemy& rhs) {
+        Coordinate lhs_delta = lhs.world_pos - entities.player.world_pos;
+        lhs_delta *= lhs_delta;
+
+        Coordinate rhs_delta = rhs.world_pos - entities.player.world_pos;
+        rhs_delta *= rhs_delta;
+        
+        return lhs_delta.x + lhs_delta.y < rhs_delta.x + rhs_delta.y;
+    });
+
+    Player& p = entities.player;
+
+    // despawn enemies
+    // for (int8_t i = entities.enemy_count - 1; i >= 0; --i) {
+    //     Coordinate dist = entities.enemies[i].world_pos - p.world_pos;
+    //     dist *= dist;
+
+    //     if (dist.x + dist.y > DESPAWN_DISTANCE) {
+    //         --entities.enemy_count;
+    //     }
+    // }
+
+    // movement
+    for (uint8_t i = 0; i < entities.enemy_count; ++i) {
+        Enemy& e = entities.enemies[i];
+        
+        Coordinate delta = p.world_pos - e.world_pos;
+        GLfloat inv_hip = quake_rsqrt(delta.x * delta.x + delta.y * delta.y);
+
+        GLfloat dist = std::min(e.speed, 1.0f / quake_rsqrt(delta.x * delta.x + delta.y * delta.y));
+
+        delta = {dist * inv_hip * delta.x, dist * inv_hip * delta.y};
+        e.world_pos += delta;
+
+        // collision
+        for (uint8_t j = 0; j < i; ++j) {
+            Enemy& e2 = entities.enemies[j];
+            
+            if (!colliding(e, e2)) continue;
+
+            Coordinate diff = e.world_pos - e2.world_pos;
+
+            Coordinate x_shift, y_shift;
+
+            if (diff.x > 0.0f) {
+                x_shift.x = e2.hitbox_width - diff.x;
+            } else {
+                x_shift.x = -(e2.hitbox_width + diff.x);
+            }
+            x_shift.y = x_shift.x * (diff.y / diff.x);
+
+            if (diff.y > 0.0f) {
+                y_shift.y = e2.hitbox_height - diff.y;
+            } else {
+                y_shift.y = -(e2.hitbox_height + diff.y);
+            }
+            y_shift.x = y_shift.y / (diff.y / diff.x);
+
+            Coordinate delta_x = e.world_pos + x_shift - entities.player.world_pos;
+            delta_x *= delta_x;
+
+            Coordinate delta_y = e.world_pos + y_shift - entities.player.world_pos;
+            delta_y *= delta_y;
+
+            e.world_pos -= delta;
+            if (delta_x.x + delta_x.y < delta_y.x + delta_y.y) {
+                delta += x_shift;
+            } else {
+                delta += y_shift;
+            }
+            e.world_pos += delta;
+        }
+
+        e.world_pos -= delta;
+        e.delta_pos = delta;
+    }
+}
 
 void gm::update_vbo(Entities& entities) {
     std::span<GLfloat> vbo_data = entities.vbo_data;
