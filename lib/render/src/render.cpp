f6d9f54a38424236fbba178a9c77c10da20c4426
LucasS2002 <sauerlucas@edu.unisinos.br>
2024-10-15 14:21:55 -0300

    moving glActiveTexture call to texture loading function

--- lib/render/src/render.cpp
+++ lib/render/src/render.cpp
@@ -10,7 +10,6 @@ void gm::render(uint64_t time_delta, Entities& entities, Background& background,
     
     // background
     glUseProgram(background.shader_program);
-    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, background.texture);
     glBindVertexArray(background.vao);
 
@@ -20,22 +19,17 @@ void gm::render(uint64_t time_delta, Entities& entities, Background& background,
 
     // entities
     glUseProgram(entities.shader_program);
-    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, entities.texture);
     glBindVertexArray(entities.vao);
+
     glUniform1f(entities.u_time_delta, time);
 
     glDrawElements(GL_TRIANGLES, 6*(entities.enemy_count), GL_UNSIGNED_SHORT, reinterpret_cast<const void*>(6*sizeof(GLshort)));
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
 
-    glBindVertexArray(0);
-    glBindTexture(GL_TEXTURE_2D, 0);
-
+    // healthbar
     glUseProgram(healthbar.shader_program);
-
-    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, healthbar.texture);
-
     glBindVertexArray(healthbar.vao);
 
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
