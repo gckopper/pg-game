1002f99ccc98f9193b406460c4fde1086c165628
Gabriel <41166074+gckopper@users.noreply.github.com>
2024-10-16 03:08:34 -0300

    adding gamepad support

--- lib/input/src/input.cpp
+++ lib/input/src/input.cpp
@@ -9,6 +9,7 @@ void gm::key_callback(GLFWwindow *window, int key, int scancode, int action, int
         return;
     }
     const int press = 2 * (action == GLFW_PRESS) - 1;
+    input.is_gamepad = false;
     switch (key) {
     case GLFW_KEY_W:
     case GLFW_KEY_UP:
@@ -36,19 +37,32 @@ void gm::key_callback(GLFWwindow *window, int key, int scancode, int action, int
 }
 
 gm::Input gm::get_input(Player& player) {
+    constexpr auto getGamepadInput = []() -> void {
+        if (!glfwJoystickIsGamepad(GLFW_JOYSTICK_1)) {
+            return;
+        }
+        GLFWgamepadstate state;
+        if (!glfwGetGamepadState(GLFW_JOYSTICK_1, &state)) {
+            return;
+        }
+        input.is_gamepad |= state.buttons[GLFW_GAMEPAD_BUTTON_A];
+        if (!input.is_gamepad) {
+            return;
+        }
+        input.attack = state.buttons[GLFW_GAMEPAD_BUTTON_A];
+        input.movement.x = state.axes[GLFW_GAMEPAD_AXIS_LEFT_X];
+        input.movement.y = -state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y];
+        if (std::abs(input.movement.x) < 0.1) input.movement.x = 0.f;
+        if (std::abs(input.movement.y) < 0.1) input.movement.y = 0.f;
+    };
+
+    getGamepadInput();
     gm::Input result = input;
 
     result.movement.x = std::clamp(result.movement.x, -1.0f, 1.0f);
     result.movement.y = std::clamp(result.movement.y, -1.0f, 1.0f);
 
-    if (std::abs(result.movement.x) < 0.1f) {
-        result.movement.x = 0.0f;
-    }
-    if (std::abs(result.movement.y) < 0.1f) {
-        result.movement.y = 0.0f;
-    }
-
-    if (result.movement.x != 0.0f && result.movement.y != 0.0f) {
+    if (!input.is_gamepad && result.movement.x != 0.0f && result.movement.y != 0.0f) {
         result.movement.x *= 0.7071067811865476f;
         result.movement.y *= 0.7071067811865476f;
     }
